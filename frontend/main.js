ad823eb7557dfeb97e28579cf569b1c0af19bf59
Marcus <biebermarcus1@gmail.com>
2025-03-04 10:50:26 +0100

    first commit

---
+++ frontend/main.js
@@ -0,0 +1,29 @@
+Array
+  .from(document.getElementsByTagName('input'))
+  .forEach(element => {
+    element.addEventListener('change', updateUI);
+  });
+
+function updateUI() {
+  // Logik zur Aktualisierung des UI hier
+  const weight = document.getElementById('weight').value;
+  const height = document.getElementById('height').value;
+  const bmi = calculateBMI(weight, height);
+  console.log("weight: %s, height: %s, bmi: %s", weight, height, bmi);
+}
+
+function calculateBMI(weight, height) {
+  // Logik zur Berechnung des BMI hier
+  const heightInMeters = height / 100; // Umrechnung von Zentimetern in Meter
+  const bmi = Math.round(weight / (heightInMeters * heightInMeters));
+  return bmi;
+}
+
+if (typeof module !== "undefined" && module.exports) {
+  // this if from nodejs
+  // this code will be executed in nodejs
+  // not in the browser
+  module.exports = { calculateBMI };
+} else {
+  console.log("Frontend-Modul geladen");
+}
